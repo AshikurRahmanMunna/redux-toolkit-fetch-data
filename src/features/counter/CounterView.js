57abb1b972163042bfd564328839a25140537cf3
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-07-11 19:02:44 +0600

    All done

---
+++ src/features/counter/CounterView.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increaseByAmount, increment, reset } from './counterSlice';
+
+const CounterView = () => {
+    const count = useSelector(state => state.counter.count);
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={() => dispatch(increment())}>INCREMENT</button>
+            <button onClick={() => dispatch(decrement())}>DECREMENT</button>
+            <button onClick={() => dispatch(reset())}>RESET</button>
+            <button onClick={() => dispatch(increaseByAmount(5))}>IncreaseBy5</button>
+        </div>
+    );
+};
+
+export default CounterView;
